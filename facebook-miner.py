fe9a87799b60d26fb661002dce8a9106e9db429a
azdkj532 <kanade123321@gmail.com>
2014-09-25 20:47:41 +0800

    api syntax not in hard code

--- facebook-miner.py
+++ facebook-miner.py
@@ -1,5 +1,6 @@
 import facebook
 access_token = input( "Please input an usable token:\n")
+APISyntax    = input( "Please input your APISyntax:\n")
 fb = facebook.GraphAPI( access_token )
 
 def recursive( source, syntax ):
@@ -9,13 +10,12 @@ def recursive( source, syntax ):
     try:
         next_page = articles['paging']['next']
     except KeyError:
-        print("test")
         raise StopIteration
     else:
         yield from recursive( source, next_page[32:] )
 
 record = open( "record.txt", "w")
-for article in recursive( fb, "314357341996385/feed?fields=id,message" ):
+for article in recursive( fb, APISyntax ):
     record.write( str(article) )
     record.write("\n")
 record.close()
