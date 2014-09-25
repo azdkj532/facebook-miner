c014fb17564da75b49e508d77ac5685532eec455
azdkj532 <kanade123321@gmail.com>
2014-09-25 18:40:11 +0800

    Done

--- facebook-miner.py
+++ facebook-miner.py
@@ -1,1 +1,21 @@
-print("hello world")
+import facebook
+access_token = input( "Please input an usable token:\n")
+fb = facebook.GraphAPI( access_token )
+
+def recursive( source, syntax ):
+    articles = source.get_object( syntax )
+    for article in articles['data']:
+        yield article
+    try:
+        next_page = articles['paging']['next']
+    except KeyError:
+        print("test")
+        raise StopIteration
+    else:
+        yield from recursive( source, next_page[32:] )
+
+record = open( "record.txt", "w")
+for article in recursive( fb, "170451929692360/feed?fields=id,message" ):
+    record.write( str(article) )
+    record.write("\n")
+record.close()
